1b12d8e89c798f78a20a487db51f0ac6f7049416
devdank <danieluitalmere@gmail.com>
2023-01-05 19:49:13 +0100

    jump added
    groundcheck sloom/buggy

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -3,24 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 12f;
-    public float gravity = -9.81f;
+    public float gravity = -0.81f;
     public float jump = 1f;
 
     public Transform groundCheck;
-    public float groundDistance = 0.4f;
+    public float groundDistance = 0.3f;
     public LayerMask groundMask;
-      Vector3 velocity;
-    bool isGrounded;
+    Vector3 velocity;
+    public bool isGrounded;
 
     // Update is called once per frame
+
+    
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
+        //isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         if(isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
@@ -35,11 +38,19 @@ public class PlayerMovement : MonoBehaviour
 
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
+            UnityEngine.Debug.Log("jumped and is grounded");
             velocity.y = Mathf.Sqrt(jump * -2f * gravity);
+            isGrounded = false;
         }
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+        UnityEngine.Debug.Log("grounded collision check");
     }
 }
